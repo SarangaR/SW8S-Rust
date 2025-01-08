9582841bf2e731542cd956aad9b4732ad689bea0
SarangaR <59266397+SarangaR@users.noreply.github.com>
2025-01-08 01:30:11 +0000

    Deploying to gh-pages from @ SarangaR/SW8S-Rust@2ae7e3ed51fda6017c9a5cb52eef20bf60b24afe 🚀

--- docs/main/trait.impl/alloc/task/trait.Wake.js
+++ docs/main/trait.impl/alloc/task/trait.Wake.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["parking",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/alloc/task/trait.Wake.html\" title=\"trait alloc::task::Wake\">Wake</a> for <a class=\"struct\" href=\"parking/struct.Inner.html\" title=\"struct parking::Inner\">Inner</a>"]]]]);
+    var implementors = Object.fromEntries([["parking",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/alloc/task/trait.Wake.html\" title=\"trait alloc::task::Wake\">Wake</a> for <a class=\"struct\" href=\"parking/struct.Inner.html\" title=\"struct parking::Inner\">Inner</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
